456d9d4b6c7ab7d05d527a171d6b99e14c0a5a62
Adyarla <48650453+Adyarla@users.noreply.github.com>
2019-04-07 17:53:33 -0300

    Add files via upload

---
+++ Genes_1/src/Genes.java
@@ -0,0 +1,100 @@
+
+public class Genes extends javax.swing.JFrame {
+
+  
+    public Genes() {
+        initComponents();
+    }
+  // Codigo     'sujo' criado a partir das criações dos botoes    
+  
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btnStart = new javax.swing.JButton();
+        btnSair = new javax.swing.JButton();
+        lblNome_Jogo = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1180, 720));
+        setSize(new java.awt.Dimension(1180, 720));
+
+        btnStart.setText("START");
+        btnStart.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnStartMouseClicked(evt);
+            }
+        });
+
+        btnSair.setText("SAIR");
+        btnSair.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnSairMouseClicked(evt);
+            }
+        });
+
+        lblNome_Jogo.setFont(new java.awt.Font("Wide Latin", 1, 36)); // NOI18N
+        lblNome_Jogo.setText("GENES");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(399, 399, 399)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(btnStart, javax.swing.GroupLayout.DEFAULT_SIZE, 82, Short.MAX_VALUE)
+                            .addComponent(btnSair, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(340, 340, 340)
+                        .addComponent(lblNome_Jogo)))
+                .addContainerGap(181, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(75, 75, 75)
+                .addComponent(lblNome_Jogo)
+                .addGap(80, 80, 80)
+                .addComponent(btnStart, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(26, 26, 26)
+                .addComponent(btnSair, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(100, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnStartMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnStartMouseClicked
+        // Clicando no start abra o formulario Jogo_Inicial
+        new Jogo_Inicial().setVisible(true);
+        
+    }//GEN-LAST:event_btnStartMouseClicked
+
+    private void btnSairMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSairMouseClicked
+        // ainda vou configurar o botao para fechar o formulario
+
+           
+    }//GEN-LAST:event_btnSairMouseClicked
+
+   
+    public static void main(String args[]) { //Main
+     
+        java.awt.EventQueue.invokeLater(new Runnable() { //Metodo que extends de uma Thread
+            public void run() {
+                new Genes().setVisible(true);
+            }
+            
+      
+            
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnSair;
+    private javax.swing.JButton btnStart;
+    private javax.swing.JLabel lblNome_Jogo;
+    // End of variables declaration//GEN-END:variables
+}
